94bd6f0eab27729d5a70f89db64b38e5540feee7
unknown <kamaumike2013@gmail.com>
2018-10-12 01:21:57 +0300

    [Feature #161142201] Add addProduct() method to add the products

--- UI/js/script.js
+++ UI/js/script.js
@@ -17,3 +17,25 @@ function openPage(evt, selectedPage) {
 
 // Get the default page to be rendered and click on the button that renders it
 document.getElementById("defaultPage").click();
+
+//Function for adding products
+function addProduct() {
+    var table = document.getElementById("productTable");
+    var row = table.insertRow(4);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    var cell4 = row.insertCell(3);
+    var cell5 = row.insertCell(4);
+    var cell6 = row.insertCell(5);
+    var cell7 = row.insertCell(6);
+    var cell8 = row.insertCell(7);                    
+    cell1.innerHTML = "4";
+    cell2.innerHTML = "Krackles";
+    cell3.innerHTML = "Snacks";
+    cell4.innerHTML = "2000";
+    cell5.innerHTML = "50";
+    cell6.innerHTML = "Ksh: 30";
+    cell7.innerHTML = "<a href=\"#\">Edit</a>";
+    cell8.innerHTML = "<a href=\"#\">Delete</a>";
+}
